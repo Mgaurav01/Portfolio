488c596a770d9b5f9944744298b69d1412e0cf6f
Mgaurav01 <mazumderg537@gmail.com>
2022-10-22 13:34:59 +0530

    commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer = (props: Props) => {
           <div className='flex items-center justify-center'>
             <img 
             className='h-10 w-9 rounded-full filter grayscale-0 hover:grayscale cursor-pointer'
-            src='https://lh3.googleusercontent.com/drive-viewer/AJc5JmREZNnKv4wK1SvqxrL8U0UuODvTspfHTkyvZJu2ZsJheXnKhcyylCFiKIBWD0ap3PIoQLc70Tw=w1920-h892'
+            src='https://cdn.onlinewebfonts.com/svg/img_213460.png'
             alt=''
             />
           </div>
